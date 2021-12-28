9ac2b29765670e2578344a503aa4ba2460858c6b
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-12-28 18:47:05 -0300

    Criando a logica para aparecer os dados criados que armazenam os links

--- components/main.jsx
+++ components/main.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import links from '../data/links'
 
 export default function Main() {
+  const [show, setShow] = useState(true);
+  const [content, setContent] = useState([]);
+
+  function handleClick(contentLink){
+    setContent(contentLink);
+    setShow(false);
+  }
+
   return (
     <div>
-      Main
+      {
+        !show &&
+          <button 
+            onClick={() => setShow(true)}
+            className="m-10"
+          >
+            Voltar
+          </button>
+      }
+      { show ?
+
+        links.map((item, idx) =>
+          <button 
+            key={idx}
+            className="m-10"
+            onClick={() => handleClick(item.content)}
+          >
+            {item.name}
+          </button>
+        )
+        
+        :
+
+        content.map((item, idx) => 
+          <a 
+            key={idx}
+            target="_blank"
+            href={item.endereco}
+            className="m-10"
+          >
+            {item.name}
+          </a>
+        )
+      
+      }
     </div>
   )
 }
